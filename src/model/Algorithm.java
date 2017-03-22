bd3a5895a7924eb7f39b55a109e924e9fd40bd6a
qwerty12794 <onzin09@gmail.com>
2017-03-22 12:28:51 +0100

    added model-view-controller; trying to make a board with two dimentional array

--- src/model/Algorithm.java
+++ src/model/Algorithm.java
@@ -1,4 +1,4 @@
-package algorithm;
+package model;
 
 public class Algorithm {
 }
---
+++ src/model/Board.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.Random;
+
+public class Board {
+    int rows = 3;
+    int collumns = 3;
+    int[][] boardArray = new int[rows][collumns];
+    public Board(){
+        fillArray();
+    }
+
+    public void fillArray(){
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<collumns;j++){
+//                boardArray[i][j] = c;
+                makeRandomChar();
+//                System.out.println(Arrays.toString(boardArray));
+            }
+        }
+
+    }
+
+    public void makeRandomChar(){
+        Random random = new Random();
+        int randomInt = (int)(random.nextInt()*26);
+        char base = (randomInt<26 ? 'A':'a');
+        System.out.println(base);
+//        return (char) (base+random % 26);
+    }
+}
