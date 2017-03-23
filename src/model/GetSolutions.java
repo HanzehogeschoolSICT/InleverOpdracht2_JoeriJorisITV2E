a7d71d54114ebd399750b8b1af0dd75c4d9b459c
qwerty12794 <onzin09@gmail.com>
2017-03-23 15:18:45 +0100

    Wat er moet gebeuren in get solutions

--- src/model/GetSolutions.java
+++ src/model/GetSolutions.java
@@ -6,10 +6,20 @@ import java.io.FileReader;
 public class GetSolutions {
 
     public GetSolutions(){
+        //voor elke letter combinatie van het bord
+            //kijk of er woorden beginnen met die letter
+            //if true:
+                //geef terug:en of er een hit is van een volledig woord
+                //zoek door als er nog geen volledige hit is
+            //en dan moet die dat dus ook van het child ding doen met die combinatie
+    }
+
+    private void backtrackFunction(){
+        //als er geen optie meer is; stop met zoeken
 
     }
 
-    public void checkInWordlist(String var){
+    private void checkInWordlist(String var){
         try(BufferedReader br = new BufferedReader(new FileReader("src/WoordenLijst.txt"))){
             String line;
             while ((line = br.readLine())!=null){
