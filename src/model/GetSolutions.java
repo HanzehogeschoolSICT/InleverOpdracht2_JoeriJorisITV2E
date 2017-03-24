3428b18915743d3dc7b5a2697f9d4648662134a9
qwerty12794 <onzin09@gmail.com>
2017-03-24 09:44:37 +0100

    Wat er moet gebeuren in get solutions

--- src/model/GetSolutions.java
+++ src/model/GetSolutions.java
@@ -6,16 +6,16 @@ import java.io.FileReader;
 public class GetSolutions {
 
     public GetSolutions(){
-        //voor elke letter combinatie van het bord
-            //kijk of er woorden beginnen met die letter
-            //if true:
-                //geef terug:en of er een hit is van een volledig woord
-                //zoek door als er nog geen volledige hit is
-            //en dan moet die dat dus ook van het child ding doen met die combinatie
     }
 
     private void backtrackFunction(){
-        //als er geen optie meer is; stop met zoeken
+        //BASECASE: als er geen optie meer is; stop met zoeken
+        //voor elke letter combinatie van het bord
+            //kijk of er woorden beginnen met die letter
+                //if true:
+                    //geef terug:en of er een hit is van een volledig woord
+                    //zoek door als er nog geen volledige hit is
+                //en dan moet die dat dus ook van het child ding doen met die combinatie
 
     }
 
@@ -24,7 +24,10 @@ public class GetSolutions {
             String line;
             while ((line = br.readLine())!=null){
                 if (line.matches("^"+var+".*")){
-                System.out.println(line);}
+                    System.out.println("half hit: "+line);
+                } else if(line.matches(var)) {
+                    System.out.println("full hit: "+line);
+                }
             }
             //TODO: Er moet nog gecheckt worden op variablen, en niet direct geprint worden; volledige hits pas printen
 
